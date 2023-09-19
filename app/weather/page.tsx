02977c794c57837929be6f90dfa50c23a1631d35
MD IMTIAZ AHMED <50336934+imtiaz2564@users.noreply.github.com>
2023-09-20 00:35:59 +0200

    initial commit

---
+++ app/weather/page.tsx
@@ -0,0 +1,22 @@
+const getPostsData = async () => {
+  const res = await fetch("http://localhost:4000/weather");
+  return res.json();
+};
+
+export default async function posts() {
+  const posts = await getPostsData();
+  console.log(posts);
+  return (
+    <div>
+      {
+        posts.map((post:any) => {
+          return <ul>
+            <li>Temperature: {post.temperature}°C</li>
+            <li>Condition: {post.condition}</li></ul>;
+        })
+      }
+      
+    </div>
+  );
+
+}

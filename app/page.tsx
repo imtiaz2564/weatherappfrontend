a8e9643e1a1a0ff36f38fedf535469594529ee9f
MD IMTIAZ AHMED <50336934+imtiaz2564@users.noreply.github.com>
2023-09-25 00:52:01 +0200

    changed design

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,28 @@ export default async function post() {
   const data = await getData();
   console.log(data);
   return (
-    <div>
-      <h3>Random Weather Information</h3>
-      {
-        data.map((post:any) => {
-          return <ul>
-            <li>Temperature: {post.temperature}°C</li>
-            <li>Condition: {post.condition}</li></ul>;
-        })
-      }
+    <div className="weather-app-wrapper">
+    <div className="weather-app-content">
+       <h3>Random Weather Information</h3>
+       {
+         data.map((post:any) => {
+           return <ul>
+             <li>Temperature: {post.temperature}°C</li>
+             <li>Condition: {post.condition}</li></ul>;
+         })
+       }
       
     </div>
+    </div>
+  //   <div className="weather-app-wrapper">
+  //   <div className="weather-app-content">
+  //     <h3>Random Weather Information</h3>
+  //   <ul>
+  //     <li>Temperature: 38°C</li>
+  //     <li>Condition: Rainy</li>
+  //   </ul>
+  //   </div>
+  // </div>
   );
 
 }
